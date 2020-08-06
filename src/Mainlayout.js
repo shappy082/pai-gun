2d1a09635f70218ff80ce6d7b140301d0fd273a4
papawich.in <papawich.in@baac.or.th>
2020-08-06 08:25:44 +0700

    topbar

--- src/Mainlayout.js
+++ src/Mainlayout.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Row } from 'antd';
+import AppTopBar from './components/AppTopBar'
 
 class Mainlayout extends React.Component {
     render() {
         return (
             <div>
-                This is Main Page.
+                <Row>
+                    <AppTopBar page="Main Page" />
+                </Row>
+                <div>
+                    This is Main Page.
+            </div>
             </div>
         )
     }
