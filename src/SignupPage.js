a9134a059ec0590ae81e5f3d6f44facc786a1100
chumsaeng <chumsaeng.do@baac.or.th>
2020-08-21 18:34:36 +0700

    add SignupPage

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -1,21 +1,101 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Input, Col, Form, Button } from "antd";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "antd/dist/antd.css";
+import { HomeOutlined } from "@ant-design/icons";
 
 class SignupPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  onFinish = async (values) => {
+    //var tagArray = values.tag.split(",");
+    try {
+      const result = await axios.post(
+        process.env.REACT_APP_API_URL + "/user/signup",
+        {
+         // tag: tagArray,
+          username: values.username,
+          password: values.password,
+          name: values.name
         }
-    }
+      );
+      console.log(result);
 
-    render() {
-        return (
-            <Row>
-                <Col>Sign Up Page</Col>
-            </Row>
-        )
+    } catch (err) {
+      alert("data wrong.");
     }
+  };
+  render() {
+    return (
+      <Row>
+        <Col>
+          <Form
+            name="addLocation"
+            className="addLocation-form"
+            style={{ width: 300 }}
+            initialValues={{
+              remember: false,
+            }}
+            onFinish={this.onFinish}
+          >
+            <Form.Item
+              name="username"
+              label="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input USERNAME ",
+                },
+              ]}
+            >
+              <Input
+                prefix={<HomeOutlined className="site-form-item-icon" />}
+                placeholder="username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input password!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<HomeOutlined className="site-form-item-icon" />}
+                placeholder="password"
+              />
+            </Form.Item>
+            <Form.Item
+              name="name"
+              label="name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input name!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<HomeOutlined className="site-form-item-icon" />}
+                placeholder="name,name,name"
+              />
+            </Form.Item>
+            <Form.Item name="submit">
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default SignupPage;
